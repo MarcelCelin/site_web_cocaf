4b305008f7e79cbc32efc97b2743c13efbe528b5
Stephane Junior Essoh Betote <essohsteph@yahoo.ca>
2026-04-16 12:07:34 +0200

    footer with facebook,contact, mail and location icons with functionalities and enhancements

--- site-web-cocaf/src/app/shared/components/footer/footer.ts
+++ site-web-cocaf/src/app/shared/components/footer/footer.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  activePopup: 'contacts' | 'email' | null = null;
 
+  email = 'contact@monentreprise.com'
+
+  contacts = [
+    {name: 'Diacre Joel', phone: '+491725887931'},
+    {name: 'Bertrand Momo', phone: '+4917646682733'}
+  ];
+
+  toggleContacts(event?: Event) {
+    if (event) {
+      event.stopPropagation();
+    }
+    this.activePopup = this.activePopup === 'contacts' ? null: 'contacts'; 
+  }
+
+  call(contact: any, event: Event) {
+    event.stopPropagation();
+    window.location.href = `tel:${contact.phone}`;
+  }
+
+  openEmailPopup(event: Event) {
+    event.stopPropagation();
+    this.activePopup = this.activePopup === 'email' ? null: 'email';
+  }
+
+  sendMail(event: Event) {
+    event.stopPropagation();
+    window.location.href = `mailto:${this.email}`;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    this.activePopup = null;
+  }
 }
